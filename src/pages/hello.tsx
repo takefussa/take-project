e88c3e3fcc48b6edce972e92770eabea9b1498e3
takefussa <ryouse523@gmail.com>
2024-10-29 15:45:29 +0900

    second commit

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -7,7 +7,7 @@ export default function Hello() {
   useEffect(() => {
     async function getMessage() {
       try {
-        const url = "https://vercel.com/new/takefussas-projects/success?developer-id=&external-id=&redirect-url=&branch=main&deploymentUrl=fast-dx773j3f0-takefussas-projects.vercel.app&projectName=fast-api&s=https%3A%2F%2Fgithub.com%2Ftakefussa%2FFastAPI&gitOrgLimit=&hasTrialAvailable=1&totalProjects=1";
+        const url = "https://fast-api-ten-gamma.vercel.app/?vercelToolbarCode=n1YCV5VCd2csfG-";
         const res = await axios.get(url);
         setMessage(res.data.message);
       } catch (err) {
