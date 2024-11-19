17b7a2cafa1dceece121ca7f0b50f3fb89333aeb
takefussa <ryouse523@gmail.com>
2024-11-19 18:33:57 +0900

    commit

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,24 @@
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+// );
+
+// export default supabase;
+
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
-);
+// 環境変数を取得
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// 環境変数のチェック
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase URL or ANON KEY in environment variables');
+}
+
+// Supabaseクライアントの作成
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
